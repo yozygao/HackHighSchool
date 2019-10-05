3d24b997d7128abb41bfc5e48edb6259d2592a96
unknown <yozygao@gmail.com>
2019-10-05 11:24:26 -0700

    hi

---
+++ 00_daenerys.py
@@ -0,0 +1,9 @@
+name= input("Who goes there? ")
+if name== 'DHTFHNUQARFMQMKGSPRLRSKBCMD':
+    print('Welcome, Daenerys')
+elif name== 'Dany':
+    print('Dany')
+elif name!= 'DHTFHNUQARFMQMKGSPRLRSKBCMD':
+    print('Move along, now.')
+elif name!= 'Dany':
+    print('Move along, now.')
---
+++ 01_brainstorm.py
@@ -0,0 +1,11 @@
+import random
+clist=['Overwatch ' ,'BTS ',]
+alist=[]
+x=random.choice(list1)
+input(x)
+if x=="Overwatch ":
+    for i in range(9):
+        input("Overwatch ")
+if x=="BTS ":
+    for i in range(9):
+        input("BTS ")
