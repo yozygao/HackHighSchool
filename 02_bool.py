36623cfb080d98a1cc49897c19d604d39c9fc36f
Vincent Gao <vgao@e1z2r3p42.42.us.org>
2019-10-12 15:21:26 -0700

    Hi

--- 02_bool.py
+++ 02_bool.py
@@ -1,5 +1,9 @@
 list1=[False,True,True,None,True,None,None,False,False,None,True,False]
 list2=["or","or","or","==","!=","==","and","==","!=","and","==","or"]
 list3=[False,False,None,None,True,True,False,True,None,False,True,None]
-lst line.split(',')
-False = lst[1]
+
+
+for x in range(12):
+    b = (str(list1[x]) + " " + list2[x] + " " + str(list3[x]))
+    x=eval(b)
+    print(b+" => " + str(x))
