f6cf44532d2e794b2b26c01b3364f733e6f08a19
Vincent Gao <vgao@e1z2r3p48.42.us.org>
2019-11-16 15:16:44 -0800

    Hi

--- 02_capitals.py
+++ 02_capitals.py
@@ -1,55 +1,66 @@
+import sys
 states_capitals = {
-        "Alabama":"Montgomery"
-        "Alaska":"Juneau"
-        "Arizona":"Phoenix"
-        "Arkansas":"Little Rock"
-        "California":"Sacramento"
-        "Colorado":"Denver"
-        "Conneticut":"Hartford"
-        "Delaware":"Dover"
-        "Florida":"Tallahassee"
-        "Goergia":"Atlanta"
-        "Hawaii":"Honolulu"
-        "Idaho":"Boise"
-        "Illinois":"Springfield"
-        "Indiana":"Indianapolis"
-        "Iowa":"Des Moines"
-        "Kansas":"Topeka"
-        "Kentucky":"Frankfort"
-        "Louisiana":"Baton Rouge"
-        "Maine":"Augusta"
-        "Maryland":"Annapolis"
-        "Massachussetts":"Boston"
-        "Michigan":"Lansing"
-        "Minnesota":"St.Paul"
-        "Mississippi":"Jackson"
-        "Missouri":"Jefferson City"
-        "Montana":"Helena"
-        "Nebraska":"Lincoln"
-        "Nevada":"Carson City"
-        "New Hampshire":"Concord"
-        "New Jersey":"Trenton"
-        "New Mexico":"Santa Fe"
-        "New York":"Albany"
-        "North Carolina":"Raleigh"
-        "North Dakota":"Bismarck"
-        "Ohio":"Columbus"
-        "Oklahoma":"Oklahoma City"
-        "Oregon":"Salem"
-        "Pennysylvania":"Harrisburg"
-        "Rhode Island":"Providence"
-        "South Carolina":"Columbia"
-        "South Dakota":"Pierre"
-        "Tennessee":"Nashville"
-        "Texas":"Austin"
-        "Utah":"Salt Lake City"
-        "Vermont":"Montpelier"
-        "Virginia":"Richmond"
-        "Washington":"Olympia"
-        "West Virginia":"Charleston"
-        "Wisconsin":"Madison"
+        "Alabama":"Montgomery",
+        "Alaska":"Juneau",
+        "Arizona":"Phoenix",
+        "Arkansas":"Little Rock",
+        "California":"Sacramento",
+        "Colorado":"Denver",
+        "Conneticut":"Hartford",
+        "Delaware":"Dover",
+        "Florida":"Tallahassee",
+        "Goergia":"Atlanta",
+        "Hawaii":"Honolulu",
+        "Idaho":"Boise",
+        "Illinois":"Springfield",
+        "Indiana":"Indianapolis",
+        "Iowa":"Des Moines",
+        "Kansas":"Topeka",
+        "Kentucky":"Frankfort",
+        "Louisiana":"Baton Rouge",
+        "Maine":"Augusta",
+        "Maryland":"Annapolis",
+        "Massachussetts":"Boston",
+        "Michigan":"Lansing",
+        "Minnesota":"St.Paul",
+        "Mississippi":"Jackson",
+        "Missouri":"Jefferson City",
+        "Montana":"Helena",
+        "Nebraska":"Lincoln",
+        "Nevada":"Carson City",
+        "New Hampshire":"Concord",
+        "New Jersey":"Trenton",
+        "New Mexico":"Santa Fe",
+        "New York":"Albany",
+        "North Carolina":"Raleigh",
+        "North Dakota":"Bismarck",
+        "Ohio":"Columbus",
+        "Oklahoma":"Oklahoma City",
+        "Oregon":"Salem",
+        "Pennysylvania":"Harrisburg",
+        "Rhode Island":"Providence",
+        "South Carolina":"Columbia",
+        "South Dakota":"Pierre",
+        "Tennessee":"Nashville",
+        "Texas":"Austin",
+        "Utah":"Salt Lake City",
+        "Vermont":"Montpelier",
+        "Virginia":"Richmond",
+        "Washington":"Olympia",
+        "West Virginia":"Charleston",
+        "Wisconsin":"Madison",
         "Wyoming":"Cheyenne"
 }
 while True:
+    key_list = list(states_capitals.keys())
+    val_list = list(states_capitals.values())
     x = input("Ready: ")
-    if x == 
+    if x in list(states_capitals.values()):
+        print(key_list[val_list.index(x)])
+    if x in list(states_capitals.keys()):
+        print(states_capitals[x])
+    if x not in states_capitals.values() and states_capitals.keys():
+        if x != "Done":
+            print("Nil")
+    if x == "Done":
+        sys.exit()
